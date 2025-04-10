00673ee5de312457ac5783f28d9e1ba1694f13eb
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-10 09:33:44 +0530

    Update pypi.py

--- pypi.py
+++ pypi.py
@@ -1,7 +1,7 @@
 import requests
 from pyrogram import filters
 from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup
-from YukkiMusic import app
+from professor import app
 
 
 def get_pypi_info(package_name):
