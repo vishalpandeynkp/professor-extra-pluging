091ded43fd2436ba39d4d339cec2651850bdae5f
NOBITA BOT DEVLOPER <155904878+vishalpandeynkp@users.noreply.github.com>
2025-04-10 09:56:14 +0530

    Update whois.py

--- whois.py
+++ whois.py
@@ -1,7 +1,7 @@
 from pyrogram import filters
 from pyrogram.errors import PeerIdInvalid
 from pyrogram.types import Message, User
-from YukkiMusic import app
+from proffesor import app
 
 
 def ReplyCheck(message: Message):
